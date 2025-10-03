b259666b9d6ca56ab32293e28627f35f0277d9bc
Munch42 <41879159+Munch42@users.noreply.github.com>
2025-10-02 19:26:33 -0600

    Made the LED blink once per second

--- app/src/main.c
+++ app/src/main.c
@@ -16,17 +16,25 @@ static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 int main(void) {
     int ret;
 
+    // I think if it is not ready, it returns from main
+    // which in this context just restarts the board
     if (!gpio_is_ready_dt(&led0)){
         return -1;;
     }
 
+    // I think this function returns a status code
+    // If it is 0 it is good probably
+    // Then negative numbers for errors. In which case we
+    // return that error code which restarts main/the board
     ret = gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE);
     if (ret < 0) {
         return ret;
     }
     
     while(1) {
-        
+        gpio_pin_toggle_dt(&led0); // Toggle the led0
+
+        k_msleep(1000);
     }
 
     return 0;
