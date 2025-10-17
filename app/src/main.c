0865fe2e405305b48559d841acd2bd1912b9f1c8
Munch42 <41879159+Munch42@users.noreply.github.com>
2025-10-16 20:11:58 -0600

    Binary Counter Working

--- app/src/main.c
+++ app/src/main.c
@@ -13,6 +13,8 @@
 
 #define SLEEP_MS        1
 
+void set_counter_leds(uint8_t counter);
+
 //#define SW0_NODE        DT_ALIAS(sw0)
 //static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
 
@@ -26,13 +28,27 @@ int main(void) {
     return 0;
   }
 
+  uint8_t counter = 0;
+
   while (1) {
     if (BTN_check_clear_pressed(BTN0)) {
-      LED_toggle(LED0);
-      printk("Button 0 pressed!\n");
+      counter++;
+      set_counter_leds(counter);
     }
     k_msleep(SLEEP_MS);
   }
 
   return 0;
+}
+
+void set_counter_leds(uint8_t counter) {
+  bool led4_MSB = (counter >> 3) & 1;
+  bool led3 = (counter >> 2) & 1;
+  bool led2 = (counter >> 1) & 1;
+  bool led1_LSB = (counter >> 0) & 1;
+
+  LED_set(LED3, led4_MSB);
+  LED_set(LED2, led3);
+  LED_set(LED1, led2);
+  LED_set(LED0, led1_LSB);
 }
