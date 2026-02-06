691d908a19c721b1b05faac3098db2dc3dfd5e05
Munch42 <41879159+Munch42@users.noreply.github.com>
2026-02-05 20:17:04 -0700

    Added buttons and button callbacks

--- app/src/main.c
+++ app/src/main.c
@@ -23,6 +23,13 @@
 static const struct device *display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
 static lv_obj_t *screen = NULL; 
 
+void lv_button_callback(lv_event_t *event) {
+  lv_obj_t *data_obj = (lv_obj_t *)lv_event_get_user_data(event);
+  led_id led = *(led_id *)lv_data_obj_get_data_ptr(data_obj);
+
+  LED_toggle(led);
+}
+
 int main(void) {
   if (!device_is_ready(display_dev)) {
     return 0;
@@ -40,8 +47,26 @@ int main(void) {
     return 0;
   }
 
-  lv_obj_t *label = lv_label_create(screen);
+  /*lv_obj_t *label = lv_label_create(screen);
   lv_label_set_text(label, "Hello World!");
+
+  display_blanking_off(display_dev);*/
+  
+  for (uint8_t i = 0; i < NUM_LEDS; i++) {
+    lv_obj_t *ui_btn = lv_button_create(screen);
+    // Place the buttons in a 2x2 grid in the center of the screen
+    // matching the orientations of the LEDS on the board
+    lv_obj_align(ui_btn, LV_ALIGN_CENTER, 50 * (i % 2 ? 1 : -1), 20 * (i < 2 ? -1 : 1));
+    lv_obj_t *button_label = lv_label_create(ui_btn);
+    char label_text[10];
+    snprintf(label_text, 10, "LED %d", i);
+    lv_label_set_text(button_label, label_text);
+    lv_obj_align(button_label, LV_ALIGN_CENTER, 0, 0);
+
+    led_id led = (led_id)i;
+    lv_obj_t *data_obj = lv_data_obj_create_alloc_assign(ui_btn, &led, sizeof(led_id));
+    lv_obj_add_event_cb(ui_btn, lv_button_callback, LV_EVENT_CLICKED, data_obj);
+  }
 
   display_blanking_off(display_dev);
   while (1) {
