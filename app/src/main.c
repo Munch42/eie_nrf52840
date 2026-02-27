4f3634e5aac4bdac6795b43e90ec37f8fda73b96
Monkey042 <Monkey042042@gmail.com>
2026-02-26 20:01:33 -0700

    Added a kind of drawing that runs out of memory

--- app/src/main.c
+++ app/src/main.c
@@ -30,6 +30,21 @@ void lv_button_callback(lv_event_t *event) {
   LED_toggle(led);
 }
 
+static void draw_dot(lv_obj_t * parent, int x, int y)
+{
+    lv_obj_t * dot = lv_obj_create(parent);
+
+    lv_obj_set_size(dot, 6, 6);
+    lv_obj_set_style_radius(dot, LV_RADIUS_CIRCLE, 0);
+    lv_obj_set_style_bg_color(dot, lv_color_hex(0x0000FF), 0);
+    lv_obj_set_style_border_width(dot, 0, 0);
+
+    lv_obj_set_pos(dot, x - 3, y - 3);
+}
+
+static lv_indev_t* touch_indev;
+static lv_point_t last_point = { -1, -1 };
+
 int main(void) {
   if (!device_is_ready(display_dev)) {
     return 0;
@@ -52,7 +67,7 @@ int main(void) {
 
   display_blanking_off(display_dev);*/
   
-  for (uint8_t i = 0; i < NUM_LEDS; i++) {
+  /*for (uint8_t i = 0; i < NUM_LEDS; i++) {
     lv_obj_t *ui_btn = lv_button_create(screen);
     // Place the buttons in a 2x2 grid in the center of the screen
     // matching the orientations of the LEDS on the board
@@ -66,11 +81,33 @@ int main(void) {
     led_id led = (led_id)i;
     lv_obj_t *data_obj = lv_data_obj_create_alloc_assign(ui_btn, &led, sizeof(led_id));
     lv_obj_add_event_cb(ui_btn, lv_button_callback, LV_EVENT_CLICKED, data_obj);
-  }
+  }*/
 
   display_blanking_off(display_dev);
+
+  touch_indev = lv_indev_get_next(NULL);
+
   while (1) {
     lv_timer_handler();
+
+    if (touch_indev) {
+        lv_point_t point;
+        lv_indev_state_t state = lv_indev_get_state(touch_indev);
+
+        if (state == LV_INDEV_STATE_PRESSED) {
+            lv_indev_get_point(touch_indev, &point);
+
+            if (point.x != last_point.x || point.y != last_point.y) {
+              draw_dot(screen, point.x, point.y);
+              last_point = point;
+            }
+        } else {
+            // If the screen was not pressed/is released, reset the last point
+            last_point.x = -1;
+            last_point.y = -1;
+        }
+    }
+
     k_msleep(SLEEP_MS);
   }
   return 0;
